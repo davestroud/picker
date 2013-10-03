9c10c27db1815967d84eb8af8eda66fdbd973d71
David Stroud <david@greymatter-ag.com>
2013-10-03 11:56:16 -0500

    added clock

--- Pickers/BIDDatePickerViewController.m
+++ Pickers/BIDDatePickerViewController.m
@@ -14,6 +14,15 @@
 
 @implementation BIDDatePickerViewController
 
+- (IBAction)buttonPressed
+{
+    NSDate *selected = [self.datePicker date];
+    NSString *message = [[NSString alloc] initWithFormat: @"The date and time you selected is: %@", selected];
+                         UIAlertView *alert = [[UIAlertView alloc]
+                                               initWithTitle: @"Date and Time Selected" message:message delegate:nil cancelButtonTitle:@"Yes, I did." otherButtonTitles:nil];
+                         [alert show];
+}
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -26,7 +35,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    NSDate *now = [NSDate date];
+    [self.datePicker setDate:now animated:NO];
 }
 
 - (void)didReceiveMemoryWarning
